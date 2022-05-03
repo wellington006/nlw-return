802091b3853d68ef854aec1865451d2bff50d387
Wellington Souza Abreu <wellingtonsa006@gmail.com>
2022-05-02 21:20:06 -0400

    Initial commit - Icon with popover

---
+++ web-application/src/App.tsx
@@ -0,0 +1,12 @@
+import {Widget} from './components/Widget'
+
+
+function App() {
+  return (
+    <div>
+      <Widget/>
+    </div>
+  )
+}
+
+export default App
---
+++ web-application/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{tsx, jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        brand:{
+          500: '#8257e6'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
